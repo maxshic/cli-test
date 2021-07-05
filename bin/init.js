b7f8accd2e468dc236fbb44a16ae74a716749488
Chong Shi <shic@aohuahealth.com>
2021-07-05 18:13:37 +0800

    0.0.4

--- bin/init.js
+++ bin/init.js
@@ -2,13 +2,21 @@
 
 const inquirer = require('inquirer')
 const chalk = require('chalk')
+const execa = require('execa')
 
+const getUser = async function(){
+  let s = ''
+  await execa('npm', ['whoami']).then(res => {
+    s = res.stdout
+  }).catch(() => s = '')
+  return s
+}
 
 const promptList = [
   {
     type: 'input',
-    // message: 'Project name:',
-    message: chalk.red('preject'),
+    message: 'Project name:',
+    // message: chalk.red('preject'),
     name: 'name',
     default: "demo"
   },
@@ -22,13 +30,13 @@ const promptList = [
     type: 'input',
     message: 'Description:',
     name: 'description',
-    default: 'a demo',
+    default: 'Some description',
   },
   {
     type: 'input',
     message: 'Author:',
     name: 'author',
-    default: 'sc'
+    default: () => getUser()
   }
 ]
 
@@ -36,6 +44,13 @@ const promptList = [
 //   Welcom to demo cli!
 //   hahahah
 // `))
+
+// const execa = require('execa')
+// execa('npm', ['view', 'aohua-cli', 'versions']).then(result => {
+//   const res =  result.stdout.replace(/\s|\[|\]|\'/g, '').split(',').sort()
+//   console.log('res', res[0])
+//   console.log('cur', require('../package.json').version)
+// }).catch(err => console.log('err'))
 
 inquirer.prompt(promptList).then(answers => {
   console.log(answers)
--- bin/upgrade.js
+++ bin/upgrade.js
@@ -2,13 +2,29 @@
 
 const execa = require('execa');
 const chalk = require('chalk');
+const Listr = require('listr');
 
-execa('node', ['-v']).then(result => {
-  console.log(result.stdout);
-}).catch(err => {
-  console.log(
-    chalk.red('error!')
-  )
+new Listr([
+  {
+    title: 'check current cli version',
+    task: (ctx, task) => execa('npm', ['view', 'aohua-cli', 'versions']).then(result => {
+        const res = result.stdout.replace(/\s|\[|\]|\'/g, '').split(',').sort()
+        if(res[res.length-1] !== require('../package.json').version){
+          ctx.up = true
+        }else{
+          ctx.up = false
+          task.skip('your cli version is uptodate')
+        }
+      }).catch(() => {
+        ctx.up = false
+        task.skip('npm error')
+      })
+  },
+  {
+    title: 'upgrade cli version',
+    enabled: ctx => ctx.up === true,
+    task: () => execa('yarn', ['-v'])
+  }
+]).run().catch(err => {
+  console.log(err)
 })
-
-// console.log('upgrade!!!')
